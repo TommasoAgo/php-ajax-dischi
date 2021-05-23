0a6a9c14c694f199f22d2f1988d2673f671fc431
TommasoAgo <74262446+TommasoAgo@users.noreply.github.com>
2021-05-23 18:07:29 +0200

    Applicato filtro a versione html

--- js/script.js
+++ js/script.js
@@ -2,12 +2,17 @@ var app = new Vue(
     {
         el: "#root",
         data : {
+            selectedGenre: "",
             cds : []
         },
         methods : {
             getCds: function() {
                 axios
-                .get('http://localhost:8888/php-ajax-dischi/server.php')
+                .get('http://localhost:8888/php-ajax-dischi/server.php', {
+                    params: {
+                        genere: this.selectedGenre
+                    }
+                })
                 .then((response) => {
                     this.cds = response.data;
                 })
--- server.php
+++ server.php
@@ -4,6 +4,26 @@ include __DIR__ . '/database.php';
 
 header('Content-Type: application/json');
 
-echo json_encode($database);
+if ( isset($_GET['genere']) && $_GET['genere'] != "" ) {
+
+    $filteredDatabase = [];
+
+    foreach ($database as $riga) {
+
+        if ( strtolower($riga['genre']) == strtolower($_GET['genere']) ) {
+
+            $filteredDatabase[] = $riga;
+
+        }
+
+    }
+
+    echo json_encode($filteredDatabase);
+
+} else {
+
+    echo json_encode($database);
+
+}
 
 ?>
